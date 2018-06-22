70fe7e73de85b6fcec075571d2a8f9c5aba17cb8
jiisoo <jsmin0415@naver.com>
2018-06-22 17:34:49 +0900

    add dinosaur obj

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ void createGLUTMenus();
 
 GLuint program;
 
-Object		g_trike, g_dilo, g_allo, g_bird, g_stry;  // dinosour
+Object		g_trike, g_dilo, g_allo, g_bird, g_stry, g_dryo;  // dinosour
 
 Object    g_cctv, g_grass, g_gate, g_rock, g_wall;
 
@@ -189,6 +189,7 @@ void init()
   g_rock.load_simple_obj("./object/Rock.obj");
 //  g_wall.load_simple_obj("./object/Wall/grade.obj");
   g_stry.load_simple_obj("./object/Styracosarus/stry.obj");
+  g_dryo.load_simple_obj("./object/Dryosarus/dryo.obj");
   glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);    // for filled polygon rendering
@@ -299,6 +300,37 @@ void display()
   glUniform4fv(loc_u_light_diffuse, 1, light_diffuse);
   glUniform4fv(loc_u_light_specular, 1, light_specular);
   g_gate.draw(loc_a_vertex, loc_a_normal,
+    loc_u_material_ambient, loc_u_material_diffuse,
+    loc_u_material_specular, loc_u_material_shininess);
+
+/////////////////////dryo///////////////////////////////
+  S = kmuvcl::math::scale(4.0f, 4.0f, 4.0f);
+  R = kmuvcl::math::rotate(-90.0f, 90.0f, 0.0f, 0.0f);
+  T = kmuvcl::math::translate(-12.0f, 0.0f, -20.0f); //바꾸지말기
+  mat_Model = T * S *R;
+  mat_PVM = mat_Proj*mat_View*mat_Model;
+
+  mat_Normal(0, 0) = mat_Model(0, 0);
+  mat_Normal(0, 1) = mat_Model(0, 1);
+  mat_Normal(0, 2) = mat_Model(0, 2);
+  mat_Normal(1, 0) = mat_Model(1, 0);
+  mat_Normal(1, 1) = mat_Model(1, 1);
+  mat_Normal(1, 2) = mat_Model(1, 2);
+  mat_Normal(2, 0) = mat_Model(2, 0);
+  mat_Normal(2, 1) = mat_Model(2, 1);
+  mat_Normal(2, 2) = mat_Model(2, 2);
+
+  glUniformMatrix4fv(loc_u_pvm_matrix, 1, false, mat_PVM);
+  glUniformMatrix4fv(loc_u_model_matrix, 1, false, mat_Model);
+  glUniformMatrix4fv(loc_u_view_matrix, 1, false, mat_View);
+  glUniformMatrix3fv(loc_u_normal_matrix, 1, false, mat_Normal);
+
+  glUniform3fv(loc_u_light_vector, 1, light_vector);
+  glUniform4fv(loc_u_light_ambient, 1, light_ambient);
+  glUniform4fv(loc_u_light_diffuse, 1, light_diffuse);
+  glUniform4fv(loc_u_light_specular, 1, light_specular);
+
+  g_dryo.draw(loc_a_vertex, loc_a_normal,
     loc_u_material_ambient, loc_u_material_diffuse,
     loc_u_material_specular, loc_u_material_shininess);
 
