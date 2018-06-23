5cd60ce179ee2d0d1333d1fbe88e721d2afafb4c
kimgayeon <yellow139@naver.com>
2018-06-23 12:24:52 +0900

    add dinosour carcar

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ void mouseButton(int xCursor, int yCursor);
 
 GLuint program;
 
-Object		g_trike, g_dilo, g_allo, g_bird, g_stry, g_dryo, g_albert, g_carcar;  // dinosour
+Object		g_trike, g_dilo, g_allo, g_bird, g_stry, g_dryo, g_albert, g_carcar, g_steg;  // dinosour
 
 Object    g_cctv, g_grass, g_gate, g_rock, g_wall;
 
@@ -225,6 +225,7 @@ void init()
   g_dryo.load_simple_obj("./object/Dryosarus/dryo.obj");
   g_albert.load_simple_obj("./object/Albertosaurus/albert.obj");
   g_carcar.load_simple_obj("./object/Carcharodontosaurus/carcar.obj");
+  g_steg.load_simple_obj("./object/Stegosarus/steg.obj");
 
   glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
@@ -524,7 +525,7 @@ void display()
     loc_u_material_ambient, loc_u_material_diffuse,
     loc_u_material_specular, loc_u_material_shininess);
 
-//////////////////////albert   빨강이//////////////////////////////
+//////////////////////carcar  보라//////////////////////////////
   S = kmuvcl::math::scale(1.5f, 1.5f, 1.5f);
   R = kmuvcl::math::rotate(-90.0f, 90.0f, 0.0f, 0.0f);
   T = kmuvcl::math::translate(6.0f, 0.0f, -40.0f);
@@ -551,6 +552,36 @@ void display()
   glUniform4fv(loc_u_light_diffuse, 1, light_diffuse);
   glUniform4fv(loc_u_light_specular, 1, light_specular);
   g_carcar.draw(loc_a_vertex, loc_a_normal,
+    loc_u_material_ambient, loc_u_material_diffuse,
+    loc_u_material_specular, loc_u_material_shininess);
+
+//////////////////////steg  보라//////////////////////////////
+  S = kmuvcl::math::scale(1.5f, 1.5f, 1.5f);
+  R = kmuvcl::math::rotate(-90.0f, 90.0f, 0.0f, 0.0f);
+  T = kmuvcl::math::translate(12.0f, 0.0f, -40.0f);
+  mat_Model = T * S * R;
+  mat_PVM = mat_Proj*mat_View*mat_Model;
+
+  mat_Normal(0, 0) = mat_Model(0, 0);
+  mat_Normal(0, 1) = mat_Model(0, 1);
+  mat_Normal(0, 2) = mat_Model(0, 2);
+  mat_Normal(1, 0) = mat_Model(1, 0);
+  mat_Normal(1, 1) = mat_Model(1, 1);
+  mat_Normal(1, 2) = mat_Model(1, 2);
+  mat_Normal(2, 0) = mat_Model(2, 0);
+  mat_Normal(2, 1) = mat_Model(2, 1);
+  mat_Normal(2, 2) = mat_Model(2, 2);
+
+  glUniformMatrix4fv(loc_u_pvm_matrix, 1, false, mat_PVM);
+  glUniformMatrix4fv(loc_u_model_matrix, 1, false, mat_Model);
+  glUniformMatrix4fv(loc_u_view_matrix, 1, false, mat_View);
+  glUniformMatrix3fv(loc_u_normal_matrix, 1, false, mat_Normal);
+
+  glUniform3fv(loc_u_light_vector, 1, light_vector);
+  glUniform4fv(loc_u_light_ambient, 1, light_ambient);
+  glUniform4fv(loc_u_light_diffuse, 1, light_diffuse);
+  glUniform4fv(loc_u_light_specular, 1, light_specular);
+  g_steg.draw(loc_a_vertex, loc_a_normal,
     loc_u_material_ambient, loc_u_material_diffuse,
     loc_u_material_specular, loc_u_material_shininess);
 
